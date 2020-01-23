63dd89daad86613dbd4dbfcf515a330eef3188a3
Jennifer Miller <jmillerarcher@me.com>
2020-01-23 00:38:30 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,11 +3,12 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
 
  def change
     create_table :costumes do |t|
-    t.string :name
+      
+     t.string :name
 
-    t.float :price
-   t.string :image_url
-    t.string :size
+     t.float :price
+     t.string :image_url
+     t.string :size
      t.timestamps
     end
   end
