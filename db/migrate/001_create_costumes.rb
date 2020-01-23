29d45d497b250ee722ca2670c7a5d663db8a2cb9
Jennifer Miller <jmillerarcher@me.com>
2020-01-23 00:37:19 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,3 +1,14 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
 
 
+ def change
+# file name after the number, so in this case:	    create_table :costumes do |t|
+# 002_create_costumes.rb becomes class CreateCostumes	      t.string :name
+
+      t.float :price
+# Define a change method in which to do the migration	      t.string :image_url
+# In this change method, create columns with the correct names and 	      t.string :size
+# value types according to the spec 	      t.timestamps
+    end
+  end
+end
